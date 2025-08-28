259720baf3ba2a0a9216e548dae7d30dd5f5f7c2
Gantzos Panos <33899942+JIKO995@users.noreply.github.com>
2025-08-28 09:27:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -409,7 +409,7 @@ Certifications: CSA, CAD, ITSM, CSM, SPM
             </div>
           </Section>
           {/* --- AI Chat Component --- */}
- <AIChat knowledge={knowledge} />
+ {/* <AIChat knowledge={knowledge} /> */}
         </main>
       </div> 
     </>
